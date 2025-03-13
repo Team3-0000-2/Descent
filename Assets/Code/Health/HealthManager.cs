6f107e613ccbf07d44e724c40908f699d6fc7475
Deva <manuelbocelli1@gmail.com>
2025-03-13 13:16:51 +0100

    fixing shooting
    Added Damage/Health manager

---
+++ Assets/Code/Health/HealthManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour
+{
+    [SerializeField] private byte type;
+    [SerializeField] private int maxHealth;
+    [SerializeField] private int currentHealth;
+
+
+    public void AddDamage(int damage)
+    {
+        switch (type)
+        {
+            case 0: //se player
+                currentHealth -= damage/2;
+                break;
+            case 1: // se nemico
+                currentHealth -= damage;
+                break;
+            default:
+                break;
+
+        }
+
+        if (currentHealth <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+} 
--- Assets/Code/Shooting/Projectile.cs
+++ Assets/Code/Shooting/Projectile.cs
@@ -3,9 +3,23 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     [SerializeField] private float _speed;
+    [SerializeField] private int damage;
     public void Update()
     {
         this.transform.Translate(this.transform.forward * _speed * Time.deltaTime);
         
     }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("enemy")|| collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<HealthManager>().AddDamage(damage);
+        }
+        Destroy(this.gameObject);
+    }
+
+
+
+
 }
